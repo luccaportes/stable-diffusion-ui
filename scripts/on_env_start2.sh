ca79844a05c3e54e527ac25699c32d93d4e91a71
Lucca Portes <lucca54@gmail.com>
2022-11-13 22:46:01 -0300

    a

---
+++ scripts/on_env_start2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+source ./scripts/functions.sh
+
+printf "\n\nStable Diffusion UI\n\n"
+
+if [ -f "scripts/config.sh" ]; then
+    source scripts/config.sh
+fi
+
+if [ "$update_branch" == "" ]; then
+    export update_branch="main"
+fi
+
+if [ -f "scripts/install_status.txt" ] && [ `grep -c sd_ui_git_cloned scripts/install_status.txt` -gt "0" ]; then
+    echo "Stable Diffusion UI's git repository was already installed. Updating from $update_branch.."
+
+    cd sd-ui-files
+
+    git reset --hard
+    git -c advice.detachedHead=false checkout "$update_branch"
+    git pull
+
+    cd ..
+else
+    printf "\n\nDownloading Stable Diffusion UI..\n\n"
+    printf "Using the $update_branch channel\n\n"
+
+    if git clone -b "$update_branch" https://github.com/luccaportes/stable-diffusion-ui.git sd-ui-files ; then
+        echo sd_ui_git_cloned >> scripts/install_status.txt
+    else
+        fail "git clone failed"
+    fi
+fi
+
+rm -rf ui
+cp -Rf sd-ui-files/ui .
+cp sd-ui-files/scripts/on_sd_start.sh scripts/
+cp sd-ui-files/scripts/bootstrap.sh scripts/
+cp sd-ui-files/scripts/start.sh .
+cp sd-ui-files/scripts/developer_console.sh .
+
+./scripts/on_sd_start2.sh
+
+# read -p "Press any key to continue"
---
+++ scripts/start2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# set legacy installer's PATH, if it exists
+if [ -e "installer" ]; then export PATH="$(pwd)/installer/bin:$PATH"; fi
+
+# Setup the packages required for the installer
+scripts/bootstrap.sh || exit 1
+
+# set new installer's PATH, if it downloaded any packages
+if [ -e "installer_files/env" ]; then export PATH="$(pwd)/installer_files/env/bin:$PATH"; fi
+
+# Test the bootstrap
+which git
+git --version || exit 1
+
+which conda
+conda --version || exit 1
+
+# Download the rest of the installer and UI
+scripts/on_env_start2.sh
